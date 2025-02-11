db22f9ed21220a9ce5c3e8b9687d2884b47499d4
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-02-11 12:49:49 -0300

    Projeto iniciado e navbar feita

---
+++ blog_dtup/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+import { Outlet } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <div className='container'>
+        <Outlet/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ blog_dtup/src/components/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+        <h2>
+            <Link to={`/`}>Blog DTUP</Link>
+        </h2>
+        <ul>
+            <li>
+                <Link to={`/`}>Home</Link>
+            </li>
+            <li>
+                <Link to={`/new`} className="new-btn">Criar novo post</Link>
+            </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
