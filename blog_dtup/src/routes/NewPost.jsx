d4287387f17a1931181c6ba4282a4aae6743b1ae
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-02-15 04:01:48 -0300

    pagina post e inicio css

--- blog_dtup/src/routes/NewPost.jsx
+++ blog_dtup/src/routes/NewPost.jsx
@@ -1,8 +1,21 @@
-
+import "./NewPost.css"
 
 const NewPost = () => {
   return (
-    <div>NewPost</div>
+    <div className="new-post">
+      <h2>Escreva um novo post!</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="title">Título do post</label>
+          <input type="text" name="title" id="title" placeholder="Escreva o título do post" />  
+        </div>
+        <div className="form-control">
+          <label htmlFor="body">Conteúdo:</label>
+          <textarea name="body" id="body" placeholder="Digite o conteúdo do seu post"></textarea>
+        </div>
+        <input type="submit" value="Enviar post" className="btn-enviar" />
+      </form>
+    </div>
   )
 }
 
