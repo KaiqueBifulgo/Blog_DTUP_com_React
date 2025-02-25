54936cfa3ca8b9ef523ffddcd5bb2a10c96d8092
KaiqueBifulgo <kaiquebwork@hotmail.com>
2025-02-25 12:06:42 -0300

    Projeto criado e CRUD concluido

--- blog_dtup/src/routes/EditPost.jsx
+++ blog_dtup/src/routes/EditPost.jsx
@@ -2,18 +2,51 @@ import blogDtup from "../axios/config"
 
 import { useState, useEffect } from "react"
 
-import { useNavigate, useParams } from "react-router-dom"
+import { useParams } from "react-router-dom"
 
 import "./EditPost.css"
 
 const EditPost = () => {
 
-    const navigate = useNavigate();
-
     const [title, setTitle] = useState();
     const [body, setBody] = useState();
 
     const { id } = useParams();
+
+
+    const getPosts = async () => {
+  
+    try {
+      
+      const response = await blogDtup.get(`/posts/${id}`) 
+
+      const data = response.data;
+
+      setTitle(data.title)
+      setBody(data.body)
+
+    } catch (error) {
+      
+      console.log(error)
+
+    }
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, [])
+
+
+  const editPost = async (e) => {
+    e.preventDefault();
+
+    const post = {title, body, userId: 1}
+
+    await blogDtup.put(`/posts/${id}`, {
+      body: post
+    })
+  }
+
 
 
   return (
@@ -22,11 +55,11 @@ const EditPost = () => {
       <form onSubmit={(e) => editPost(e)}>
         <div className="form-control">
           <label htmlFor="title">Editar título do post</label>
-          <input type="text" name="title" id="title" placeholder="Escreva o novo título do post" onChange={(e) => setTitle(e.target.value)}/>  
+          <input type="text" name="title" id="title" placeholder="Escreva o novo título do post" onChange={(e) => setTitle(e.target.value)} value={title || ""}/>  
         </div>
         <div className="form-control">
           <label htmlFor="body">Editar conteúdo:</label>
-          <textarea name="body" id="body" placeholder="Edite seu post!" onChange={(e) => setBody(e.target.value)}></textarea>
+          <textarea name="body" id="body" placeholder="Edite seu post!" onChange={(e) => setBody(e.target.value)} value={body || ""}></textarea>
         </div>
         <input type="submit" value="Confirmar edição de post" className="btn-edit-enviar" />
       </form>
